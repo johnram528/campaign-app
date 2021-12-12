93317282265d1b85a080d161b7a4f267551a3ff1
johnram528 <johnram528@gmail.com>
2021-12-12 15:35:50 -0600

    initialize react app

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import { theme, ThemeProvider } from '@chakra-ui/core'
+import './App.css';
+
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+    <div className="App">
+
+    </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
