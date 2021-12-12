85094654440a66cd4b3b6363e5ceaf471a75f9e0
johnram528 <johnram528@gmail.com>
2021-12-12 15:59:44 -0600

    add campaign page and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { theme, ThemeProvider } from '@chakra-ui/core'
 import './App.css';
+import CreateCampaign from './components/pages/Create';
+import { Route, Routes } from 'react-router';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
     <div className="App">
-
+      <div className={'container'}>
+        <Routes>
+          <Route path="/" element={<CreateCampaign />} />
+          {/* <Route path={"/campaign/:campaignId"} element={<campaign />}/>
+          <Route path={"/edit/:campaignId"} element={<Edit />}/>
+          <Route path={"/create"} element={<Create />} /> */}
+        </Routes>
+      </div>
     </div>
     </ThemeProvider>
   );
