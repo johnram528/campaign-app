d4126c0b86f6d3dcd9754d696d844a640c81bde2
johnram528 <johnram528@gmail.com>
2021-12-12 20:00:15 -0600

    add emptystate component

--- client/src/components/EmptyState.tsx
+++ client/src/components/EmptyState.tsx
@@ -1,1 +1,38 @@
-export {}
+import React from 'react'
+import { Box, Heading, Stack, Text, Button } from '@chakra-ui/core'
+
+const EmptyState = (): JSX.Element => {
+  return (
+    <Box
+      backgroundColor="white"
+      ml={0}
+      mr={0}
+      borderRadius={8}
+      boxShadow="0px 4px 10px rgba(0, 0, 0, 0.05)"
+    >
+      <Box
+        backgroundColor="gray.50"
+        borderTopLeftRadius={8}
+        borderTopRightRadius={8}
+        borderBottom="1px solid"
+        borderBottomColor="gray.200"
+        height="40px"
+      />
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        p={16}
+        borderRadius={8}
+      >
+        <Heading size="lg">You haven’t added any campaigns.</Heading>
+        <Text>Let’s get started.</Text>
+        <Button>
+          Create your first campaign!
+        </Button>
+      </Stack>
+    </Box>
+  )
+}
+
+export default EmptyState
